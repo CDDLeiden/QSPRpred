ac65231ff6f9fb3275fbefd82629ed9d2dd1ada2
martin-sicho <sicho.martin@gmail.com>
2023-02-12 19:09:13 +0100

    add regression plots

--- qsprpred/plotting/tests.py
+++ qsprpred/plotting/tests.py
@@ -8,13 +8,15 @@ import os
 from unittest import TestCase
 
 import pandas as pd
+from matplotlib.axes import SubplotBase
 from matplotlib.figure import Figure
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
 from qsprpred.models.models import QSPRsklearn
 from qsprpred.models.tasks import ModelTasks
 from qsprpred.models.tests import ModelDataSets
 from qsprpred.plotting.classification import ROCPlot, MetricsPlot
+from qsprpred.plotting.regression import CorrelationPlot
 
 
 class ROCPlotTest(ModelDataSets, TestCase):
@@ -73,3 +75,29 @@ class MetricsPlotTest(ModelDataSets, TestCase):
         self.assertTrue(os.path.exists(f"{model.outDir}/metrics_summary.tsv"))
         self.assertTrue(os.path.exists(f"{model.outDir}/metrics_f1_score.png"))
 
+
+class CorrPlotTest(ModelDataSets, TestCase):
+
+    @staticmethod
+    def get_model(dataset, name, alg=RandomForestRegressor):
+        return QSPRsklearn(
+            name=name,
+            data=dataset,
+            base_dir=os.path.dirname(ROCPlotTest.qsprmodelspath.replace("qspr/models", "")),
+            alg=alg,
+        )
+
+    def test_plot_single(self):
+        dataset = self.create_large_dataset("test_corr_plot_single_data")
+        model = self.get_model(dataset, "test_corr_plot_single_model")
+        model.evaluate(save=True)
+        model.save()
+
+        plt = CorrelationPlot([model])
+
+        # generate metrics plot and associated files
+        axes, summary = plt.make(out_dir=model.outDir)
+        self.assertIsInstance(summary, pd.DataFrame)
+        for ax in axes:
+            self.assertIsInstance(ax, SubplotBase)
+        self.assertTrue(os.path.exists(f"{model.outDir}/corrplot.png"))
