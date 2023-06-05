e6da74f8f8e45b0c050c0e04317f6b198079845e
martin-sicho <sicho.martin@gmail.com>
2023-06-05 16:20:51 +0200

    add a few more rules and format docstrings

--- docs/style_guide.py
+++ docs/style_guide.py
@@ -1,32 +1,40 @@
 """Example file to illustrate our style guide.
 
-We try to adhere to PEP 8 (https://peps.python.org/pep-0008/#introduction) and diverge where we think is appropriate. Summary of the implemented and enforced rules:
+We try to adhere to PEP 8 (https://peps.python.org/pep-0008/#introduction) and diverge
+where we think is appropriate. Summary of the implemented and enforced rules:
 
-- Maximum line length (pep8: 79 characters, but we use 88).
+- Maximum line length (pep8: 79 characters, but we use 88). Also enforced for docstrings.
 - Use double quotes for strings.
 - Imports inside the package should be relative rather than absolute.
 - Comments/documentation
-    - Write docstrings for all (not just public!) modules, functions, classes, and methods.
-    - Use the Google docstring format (https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html)
+    - Write docstrings for all (not just public!) modules, functions, classes,
+      and methods.
+    - Use the Google docstring format
+      (https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html)
     - multi-line docstrings (pep8, trailing quotes on a new line)
     - single-line docstrings (pep8, trailing quotes on the same line)
 - Naming conventions
-    - module names (pep8: short, all-lowercase names. Underscores can be used in the module name if it improves readability.)
+    - module names (pep8: short, all-lowercase names. Underscores can be used in the
+      module name if it improves readability.)
     - class names (pep 8: CapWords)
     - class method and attribute names (own style: mixedCase, deviation from PEP 8)
-    - other variable names (pep 8: lowercase, with words separated by underscores as necessary to improve readability.)
-    - function names (pep 8: lowercase, with words separated by underscores as necessary to improve readability.)
+    - other variable names (pep 8: lowercase, with words separated by underscores as
+      necessary to improve readability.)
+    - function names (pep 8: lowercase, with words separated by underscores as necessary
+      to improve readability.)
 - White spaces
     - 4 spaces for indentation
     - two spaces between module member definitions
     - one space between class member definitions
     - no spaces between code or comments in function and method bodies
 - Type hints
-    - use type hint built-in functionality as introduced in Python 3.9 and later (https://docs.python.org/3/library/typing.html)
+    - use type hint built-in functionality as introduced in Python 3.9 and later
+      (https://docs.python.org/3/library/typing.html)
 
 Example:
 
-    Some of these rules can be enforced automatically with Autoflake by running the following on your files:
+    Some of these rules can be enforced automatically with Autoflake
+    by running the following on your files:
 
         $ black --line-length 88 style_guide.py
         $ isort --profile black style_guide.py
@@ -45,7 +53,10 @@ module_variable = "String literal"
 
 
 def function():
-    """An example function without arguments. Note the use of comments to divide the function into logical blocks, not spaces."""
+    """An example function without arguments.
+
+    Note the use of comments to divide the function into logical blocks, not spaces.
+    """
 
     # example for cycle and variable definition
     example_variable = (
@@ -63,7 +74,9 @@ def function():
 class ExampleClass:
     """Simple example class.
 
-    This class shows style patterns for classes. If a class has static/class members they should be at the top followed by built-in overrides like __init__ or __contains__. All instance specific methods follow after.
+    This class shows style patterns for classes. If a class has static/class members
+    they should be at the top followed by built-in overrides like `__init__` or
+    `__contains__`. All instance specific methods follow after.
 
     Attributes:
         shortName (str): static class attribute
@@ -73,7 +86,7 @@ class ExampleClass:
     shortName = "Some short name."  # static members
 
     class ClassInClass:
-        """Example"""
+        """Example class."""
 
     @staticmethod
     def staticMethod():
@@ -81,10 +94,10 @@ class ExampleClass:
 
     @classmethod
     def classMethod(cls):
-        """Example class method"""
+        """Example class method."""
 
     def __init__(self):
-        """The init"""
+        """The init."""
 
         self.someAttribute = "Value"
 
@@ -96,8 +109,8 @@ class ExampleClass:
         Nothing to see here.
 
         Args:
-            arg_one (str): first argument, a dictionary with string keys and float values
-            arg_two (str): second argument, a list of strings or floats
+            arg_one (str): first argument, dictionary with string keys and float values
+            arg_two (str): second argument, list of strings or floats
 
         Returns:
             stuff (str): some stuff
