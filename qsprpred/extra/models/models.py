280b573e0ff87071492f695c7d12afc9c87643da
Linde <l.schoenmaker@lacdr.leidenuniv.nl>
2023-07-27 14:52:26 +0200

    add pyboost model class

---
+++ qsprpred/extra/models/models.py
@@ -0,0 +1,198 @@
+"""Here extra model classes can be found.
+
+At the moment this contains a class for py-boost models. To add more a model class
+implementing the `QSPRModel` interface can be added, see tutorial adding_new_components.
+"""
+
+import os
+from copy import deepcopy
+from typing import Any, Optional, Type
+
+import joblib
+import numpy as np
+import pandas as pd
+from py_boost import GradientBoosting
+from sklearn.model_selection import train_test_split
+
+from ...data.data import QSPRDataset
+from ...models.interfaces import QSPRModel
+
+
+class PyBoostModel(QSPRModel):
+    """PyBoostModel class for gradient boosting with option to do multioutput and
+    customizable loss and evaluation."""
+    def __init__(
+        self,
+        base_dir: str,
+        data: Optional[QSPRDataset] = None,
+        name: Optional[str] = None,
+        parameters: Optional[dict] = None,
+        autoload=True
+    ):
+        """Initialize a QSPR model instance.
+
+        If the model is loaded from file, the data set is not required.
+        Note that the data set is required for fitting and optimization.
+
+        Args:
+            base_dir (str):
+                base directory of the model,
+                the model files are stored in a subdirectory `{baseDir}/{outDir}/`
+            data (QSPRDataset): data set used to train the model
+            name (str): name of the model
+            parameters (dict): dictionary of algorithm specific parameters
+            autoload (bool):
+                if `True`, the estimator is loaded from the serialized file
+                if it exists, otherwise a new instance of alg is created
+        """
+        super().__init__(base_dir, GradientBoosting, data, name, parameters, autoload)
+
+    @property
+    def supportsEarlyStopping(self) -> bool:
+        """Check if the model supports early stopping.
+
+        Returns:
+            (bool): whether the model supports early stopping or not
+        """
+        return False
+
+    def fit(
+        self,
+        X: pd.DataFrame | np.ndarray | QSPRDataset,
+        y: pd.DataFrame | np.ndarray | QSPRDataset,
+        estimator: Optional[Type[GradientBoosting]] = None,
+        early_stopping: bool = False,
+        **kwargs
+    ) -> GradientBoosting:
+        """Fit the model to the given data matrix or `QSPRDataset`.
+
+        Args:
+            X (pd.DataFrame, np.ndarray, QSPRDataset): data matrix to fit
+            y (pd.DataFrame, np.ndarray, QSPRDataset): target matrix to fit
+            estimator (Any): estimator instance to use for fitting
+            early_stopping (bool): if True, early stopping is used
+            kwargs: additional keyword arguments for the fit function
+
+        Returns:
+            (GzipKNNAlgorithm): fitted estimator instance
+        """
+        estimator = self.estimator if estimator is None else estimator
+        X, y = self.convertToNumpy(X, y)
+
+        if early_stopping:
+            # split cross validation fold train set into train
+            # and validation set for early stopping
+            X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1)
+            estimator.fit(X_train, y_train, eval_sets=[{"X": X_val, "y": y_val}])
+
+            return estimator, estimator.best_round
+
+        estimator.fit(X, y)
+
+        return estimator
+
+    def predict(
+        self,
+        X: pd.DataFrame | np.ndarray | QSPRDataset,
+        estimator: Any = None
+    ) -> np.ndarray:
+        """Make predictions for the given data matrix or `QSPRDataset`.
+
+        Args:
+            X (pd.DataFrame, np.ndarray, QSPRDataset): data matrix to predict
+            estimator (Any): estimator instance to use for fitting
+
+        Returns:
+            np.ndarray:
+                2D array containing the predictions, where each row corresponds
+                to a sample in the data and each column to a target property
+        """
+        estimator = self.estimator if estimator is None else estimator
+        X = self.convertToNumpy(X)
+
+        preds = estimator.predict(X)
+
+        if self.task.isClassification():
+            if preds.shape[1] == 1:
+                preds = np.concatenate((preds, 1 - preds), axis=1)
+            return np.argmax(preds, axis=1, keepdims=True)
+        else:
+            return preds
+
+    def predictProba(
+        self,
+        X: pd.DataFrame | np.ndarray | QSPRDataset,
+        estimator: Any = None
+    ) -> np.ndarray:
+        estimator = self.estimator if estimator is None else estimator
+        X = self.convertToNumpy(X)
+
+        preds = estimator.predict(X)
+        if self.task.isClassification() and preds.shape[1] == 1:
+            preds = np.concatenate((preds, 1 - preds), axis=1)
+        # if preds is a numpy array, convert it to a list
+        # to be consistent with the multiclass-multitask case
+        if isinstance(preds, np.ndarray):
+            preds = [preds]
+        return preds
+
+    def loadEstimator(self, params: Optional[dict] = None) -> object:
+        """Initialize estimator instance with the given parameters.
+
+        If `params` is `None`, the default parameters will be used.
+
+        Arguments:
+            params (dict): algorithm parameters
+
+        Returns:
+            object: initialized estimator instance
+        """
+        if params:
+            if self.parameters is not None:
+                temp_params = deepcopy(self.parameters)
+                temp_params.update(params)
+                return self.alg(**temp_params)
+            else:
+                return self.alg(**params)
+        elif self.parameters is not None:
+            return self.alg(**self.parameters)
+        else:
+            return self.alg()
+
+    def loadEstimatorFromFile(
+        self, params: Optional[dict] = None, fallback_load=True
+    ) -> object:
+        """Load estimator instance from file and apply the given parameters.
+
+        Args:
+            params (dict): algorithm parameters
+
+        Returns:
+            object: initialized estimator instance
+        """
+        path = f"{self.outPrefix}.pkl"
+        if os.path.isfile(path):
+            estimator = joblib.load(path)
+            new_parameters = self.getParameters(params)
+            if new_parameters is not None:
+                return estimator(**new_parameters)
+            else:
+                return estimator
+        elif fallback_load:
+            return self.loadEstimator(params)
+        else:
+            raise FileNotFoundError(
+                f"No estimator found at {path}, loading estimator from file failed."
+            )
+
+    def saveEstimator(self) -> str:
+        """Save the underlying estimator to file.
+
+        Returns:
+            path (str): path to the saved estimator
+        """
+        estimator_path = f"{self.outPrefix}.pkl"
+
+        joblib.dump(self.estimator, "../data/temp_model.pkl")
+
+        return estimator_path
