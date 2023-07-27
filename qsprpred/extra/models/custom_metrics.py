430816e465d3de9ed2426858d5c88a7e0cfc37da
Linde <l.schoenmaker@lacdr.leidenuniv.nl>
2023-07-27 15:19:37 +0200

    add wrappers for masked classification metrics

--- qsprpred/extra/models/custom_metrics.py
+++ qsprpred/extra/models/custom_metrics.py
@@ -1,8 +1,9 @@
 """This module holds custom metric wrappers for pyboost,
 as adapted from the tutorials in https://github.com/sb-ai-lab/Py-Boost
 """
+import cupy as cp
 import numpy as np
-from py_boost.gpu.losses.metrics import Metric
+from py_boost.gpu.losses.metrics import Metric, auc
 
 
 class NaNRMSEScore(Metric):
@@ -38,6 +39,25 @@ class NaNR2Score(Metric):
             std = np.nanvar(np.where(mask, y_true, np.nan), axis=0)
 
         return np.average(1 - err / std, weights=np.count_nonzero(mask, axis=0))
+
+    def compare(self, v0, v1):
+
+        return v0 > v1
+
+
+class NaNAucMetric(Metric):
+    "Masked AUC score"
+    def __call__(self, y_true, y_pred, sample_weight=None):
+
+        aucs = []
+        mask = ~cp.isnan(y_true)
+
+        for i in range(y_true.shape[1]):
+            m = mask[:, i]
+            w = None if sample_weight is None else sample_weight[:, 0][m]
+            aucs.append(auc(y_true[:, i][m], y_pred[:, i][m], w))
+
+        return np.mean(aucs)
 
     def compare(self, v0, v1):
 
