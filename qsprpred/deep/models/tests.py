89aeec73e1d29d656b8611fd412d6f3ba1c75d27
Linde <l.schoenmaker@lacdr.leidenuniv.nl>
2023-07-28 17:56:48 +0200

    move pyboost to deep

--- qsprpred/deep/models/tests.py
+++ qsprpred/deep/models/tests.py
@@ -10,12 +10,16 @@ from unittest import TestCase
 
 import torch
 from parameterized import parameterized
+from sklearn.impute import SimpleImputer
 
 from ...data.data import QSPRDataset
 from ...deep.models.models import QSPRDNN
 from ...deep.models.neural_network import STFullyConnected
 from ...models.tasks import TargetTasks
 from ...models.tests import ModelDataSetsMixIn, ModelTestMixIn
+from ..models.custom_loss import BCEWithNaNLoss, MSEwithNaNLoss
+from ..models.custom_metrics import NaNAucMetric, NaNR2Score, NaNRMSEScore
+from ..models.models import PyBoostModel
 
 GPUS = list(range(torch.cuda.device_count()))
 
@@ -103,4 +107,211 @@ class NeuralNet(ModelDataSetsMixIn, ModelTestMixIn, TestCase):
         )
         self.fitTest(model)
         predictor = QSPRDNN(name=alg_name, base_dir=model.baseDir)
+        self.predictorTest(predictor)
+
+
+class TestPyBoostModel(ModelDataSetsMixIn, ModelTestMixIn, TestCase):
+    """This class holds the tests for the PyBoostModel class."""
+    @staticmethod
+    def getModel(
+        name: str,
+        dataset: QSPRDataset = None,
+        parameters: dict | None = None,
+    ):
+        """Create a PyBoostModel model.
+
+        Args:
+            name (str): the name of the model
+            dataset (QSPRDataset, optional): the dataset to use. Defaults to None.
+            parameters (dict, optional): the parameters to use. Defaults to None.
+
+        Returns:
+            PyBoostModel the model
+        """
+        return PyBoostModel(
+            base_dir=f"{os.path.dirname(__file__)}/test_files/qspr/models",
+            data=dataset,
+            name=name,
+            parameters=parameters,
+        )
+
+    @parameterized.expand(
+        [
+            ("PyBoost", TargetTasks.REGRESSION, "PyBoost", params) for params in [
+                {
+                    "loss": "mse",
+                    "metric": "r2_score"
+                },
+                {
+                    "loss": MSEwithNaNLoss(),
+                    "metric": "r2_score"
+                },
+                {
+                    "loss": "mse",
+                    "metric": NaNR2Score()
+                },
+                {
+                    "loss": "mse",
+                    "metric": NaNRMSEScore()
+                },
+            ]
+        ]
+    )
+    def testRegressionPyBoostFit(self, _, task, model_name, parameters):
+        """Test model training for regression models."""
+        parameters["verbose"] = -1
+        # initialize dataset
+        dataset = self.create_large_dataset(
+            target_props=[{
+                "name": "CL",
+                "task": task
+            }],
+            preparation_settings=self.get_default_prep(),
+        )
+        # initialize model for training from class
+        model = self.getModel(
+            name=f"{model_name}_{task}",
+            dataset=dataset,
+            parameters=parameters,
+        )
+        self.fitTest(model)
+        predictor = PyBoostModel(name=f"{model_name}_{task}", base_dir=model.baseDir)
+        self.predictorTest(predictor)
+
+    @parameterized.expand(
+        [
+            (f"{'PyBoost'}_{task}", task, th, "PyBoost", params)
+            for params, task, th in (
+                ({
+                    "loss": "bce",
+                    "metric": "auc"
+                }, TargetTasks.SINGLECLASS, [6.5]),
+                ({
+                    "loss": "crossentropy"
+                }, TargetTasks.MULTICLASS, [0, 1, 10, 1100]),
+            )
+        ]
+    )
+    def testClassificationPyBoostFit(self, _, task, th, model_name, parameters):
+        """Test model training for classification models."""
+        parameters["verbose"] = -1
+        # initialize dataset
+        dataset = self.create_large_dataset(
+            target_props=[{
+                "name": "CL",
+                "task": task,
+                "th": th
+            }],
+            preparation_settings=self.get_default_prep(),
+        )
+        # test classifier
+        # initialize model for training from class
+        model = self.getModel(
+            name=f"{model_name}_{task}",
+            dataset=dataset,
+            parameters=parameters,
+        )
+        self.fitTest(model)
+        predictor = PyBoostModel(name=f"{model_name}_{task}", base_dir=model.baseDir)
+        self.predictorTest(predictor)
+
+    @parameterized.expand(
+        [
+            ("PyBoost", "PyBoost", params) for params in [
+                {
+                    "loss": "mse",
+                    "metric": "r2_score"
+                },
+                {
+                    "loss": MSEwithNaNLoss(),
+                    "metric": "r2_score"
+                },
+                {
+                    "loss": "mse",
+                    "metric": NaNR2Score()
+                },
+            ]
+        ]
+    )
+    def testRegressionMultiTaskFit(self, _, model_name, parameters):
+        """Test model training for multitask regression models."""
+        parameters["verbose"] = -1
+        # initialize dataset
+        dataset = self.create_large_dataset(
+            target_props=[
+                {
+                    "name": "fu",
+                    "task": TargetTasks.REGRESSION
+                },
+                {
+                    "name": "CL",
+                    "task": TargetTasks.REGRESSION
+                },
+            ],
+            target_imputer=SimpleImputer(strategy="mean"),
+            preparation_settings=self.get_default_prep(),
+        )
+        # test classifier
+        # initialize model for training from class
+        model = self.getModel(
+            name=f"{model_name}_multitask_regression",
+            dataset=dataset,
+            parameters=parameters
+        )
+        self.fitTest(model)
+        predictor = PyBoostModel(
+            name=f"{model_name}_multitask_regression", base_dir=model.baseDir
+        )
+        self.predictorTest(predictor)
+
+    @parameterized.expand(
+        [
+            ("PyBoost", "PyBoost", params) for params in [
+                {
+                    "loss": "bce",
+                    "metric": "auc"
+                },
+                {
+                    "loss": BCEWithNaNLoss(),
+                    "metric": "auc"
+                },
+                {
+                    "loss": "bce",
+                    "metric": NaNAucMetric()
+                },
+            ]
+        ]
+    )
+    def testClassificationMultiTaskFit(self, _, model_name, parameters):
+        """Test model training for multitask classification models."""
+        parameters["verbose"] = -1
+
+        # initialize dataset
+        dataset = self.create_large_dataset(
+            target_props=[
+                {
+                    "name": "fu",
+                    "task": TargetTasks.SINGLECLASS,
+                    "th": [0.3]
+                },
+                {
+                    "name": "CL",
+                    "task": TargetTasks.SINGLECLASS,
+                    "th": [6.5]
+                },
+            ],
+            target_imputer=SimpleImputer(strategy="mean"),
+            preparation_settings=self.get_default_prep(),
+        )
+        # test classifier
+        # initialize model for training from class
+        model = self.getModel(
+            name=f"{model_name}_multitask_classification",
+            dataset=dataset,
+            parameters=parameters,
+        )
+        self.fitTest(model)
+        predictor = PyBoostModel(
+            name=f"{model_name}_multitask_classification", base_dir=model.baseDir
+        )
         self.predictorTest(predictor)
