96b44b7ca4c3e6bbf55e61122e731305ab2ef6c7
Linde <l.schoenmaker@lacdr.leidenuniv.nl>
2023-08-03 13:18:27 +0200

    use skip marker instead

--- qsprpred/deep/models/tests.py
+++ qsprpred/deep/models/tests.py
@@ -114,7 +114,7 @@ class NeuralNet(ModelDataSetsMixIn, ModelTestMixIn, TestCase):
 class TestPyBoostModel(ModelDataSetsMixIn, ModelTestMixIn, TestCase):
     """This class holds the tests for the PyBoostModel class."""
     @staticmethod
-    @pytest.mark.gpu
+    @pytest.mark.skip(reason="requires gpu")
     def getModel(
         name: str,
         dataset: QSPRDataset = None,
@@ -159,7 +159,7 @@ class TestPyBoostModel(ModelDataSetsMixIn, ModelTestMixIn, TestCase):
             ]
         ]
     )
-    @pytest.mark.gpu
+    @pytest.mark.skip(reason="requires gpu")
     def testRegressionPyBoostFit(self, _, task, model_name, parameters):
         """Test model training for regression models."""
         parameters["verbose"] = -1
@@ -197,7 +197,7 @@ class TestPyBoostModel(ModelDataSetsMixIn, ModelTestMixIn, TestCase):
             )
         ]
     )
-    @pytest.mark.gpu
+    @pytest.mark.skip(reason="requires gpu")
     def testClassificationPyBoostFit(self, _, task, th, model_name, parameters):
         """Test model training for classification models."""
         parameters["verbose"] = -1
@@ -241,7 +241,7 @@ class TestPyBoostModel(ModelDataSetsMixIn, ModelTestMixIn, TestCase):
             ]
         ]
     )
-    @pytest.mark.gpu
+    @pytest.mark.skip(reason="requires gpu")
     def testRegressionMultiTaskFit(self, _, model_name, parameters):
         """Test model training for multitask regression models."""
         parameters["verbose"] = -1
@@ -291,7 +291,7 @@ class TestPyBoostModel(ModelDataSetsMixIn, ModelTestMixIn, TestCase):
             ]
         ]
     )
-    @pytest.mark.gpu
+    @pytest.mark.skip(reason="requires gpu")
     def testClassificationMultiTaskFit(self, _, model_name, parameters):
         """Test model training for multitask classification models."""
         parameters["verbose"] = -1
