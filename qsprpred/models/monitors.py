129050b2a775948b12627e0763bb51a83501c326
Mw. H.W. van den Maagdenberg MSc <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2023-09-20 17:26:35 +0200

    first implementation wandb

--- qsprpred/models/monitors.py
+++ qsprpred/models/monitors.py
@@ -82,3 +82,51 @@ class PrintMonitor(HyperParameterOptimizationMonitor):
         print("Score: %s" % score)
         print("Scores: %s" % scores)
         print("Predictions: %s" % predictions)
+
+class WandBMonitor(HyperParameterOptimizationMonitor):
+    def __init__(self):
+        try:
+            import wandb
+        except ImportError:
+            raise ImportError("WandBMonitor requires wandb to be installed.")
+        self.wandb = wandb
+
+        wandb.login()
+
+    def on_optimization_start(self, config: dict):
+        """Called before the hyperparameter optimization has started.
+
+        Args:
+            config (dict): configuration of the hyperparameter optimization
+        """
+        # self.wandb.init(project="qsprpred", config=config)
+
+    def on_optimization_end(self, best_score: float, best_parameters: dict):
+        """Called after the hyperparameter optimization has finished.
+
+        Args:
+            best_score (float): best score found during optimization
+            best_parameters (dict): best parameters found during optimization
+        """
+        # self.wandb.log({"best_score": best_score, "best_parameters": best_parameters})
+        # self.wandb.finish()
+
+    def on_iteration_start(self, params: dict):
+        """Called before each iteration of the hyperparameter optimization.
+
+        Args:
+            params (dict): parameters used for the current iteration
+        """
+        self.wandb.init(project="qsprpred2", config=params)
+
+    def on_iteration_end(self, score: float, scores: list[float], predictions: list[np.ndarray]):
+        """Called after each iteration of the hyperparameter optimization.
+
+        Args:
+            score (float): (aggregated) score of the current iteration
+            scores (list[float]): scores of the current iteration
+                                (e.g for cross-validation)
+            predictions (list[np.ndarray]): predictions of the current iteration
+        """
+        self.wandb.log({"score": score, "scores": scores})
+        self.wandb.finish()
