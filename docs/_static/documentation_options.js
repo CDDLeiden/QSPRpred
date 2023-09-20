a98632de7e647a57f53112d5308b98220de79f9b
martin-sicho <sicho.martin@gmail.com>
2023-09-20 11:58:30 +0200

    Adding docs to gh_pages for commit 124effb Merge: 56ea83b f889d4b Author: Dhr. Dr.ing. M. Sicho <sicho.martin@gmail.com>
        Merge branch 'dev' into 'main'

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.1.0a1.dev131+g15e3f33',
+    VERSION: '2.1.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
