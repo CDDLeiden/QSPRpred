d300968303213a1692bf6d52a8bd33e2b4ea021d
maagdenbehwvanden1 <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2023-11-01 10:36:29 +0100

    fix early stopping test

--- qsprpred/models/tests.py
+++ qsprpred/models/tests.py
@@ -862,9 +862,9 @@ class TestEarlyStopping(ModelDataSetsMixIn, TestCase):
                 self,
                 X,
                 y,
-                monitor=None,
                 estimator=None,
                 mode=EarlyStoppingMode.NOT_RECORDING,
+                monitor=None,
                 **kwargs,
             ):
                 return None, kwargs["best_epoch"]
@@ -873,12 +873,12 @@ class TestEarlyStopping(ModelDataSetsMixIn, TestCase):
         recording = EarlyStoppingMode.RECORDING
         not_recording = EarlyStoppingMode.NOT_RECORDING
         # epochs are recorded as self.earlyStopping.mode is set to RECORDING
-        _ = test_obj.test_func(None, None, None, None, best_epoch=29)
+        _ = test_obj.test_func(None, None, None, best_epoch=29)
         # epochs are not recorded as mode is set to NOT_RECORDING in the decorator
-        _ = test_obj.test_func(None, None, None, None, not_recording, best_epoch=49)
+        _ = test_obj.test_func(None, None, None, not_recording, best_epoch=49)
         self.assertEqual(test_obj.earlyStopping.mode, not_recording)
         # epochs are recorded as mode is now set to RECORDING in the decorator
-        _ = test_obj.test_func(None, None, None, None, recording, best_epoch=39)
+        _ = test_obj.test_func(None, None, None, recording, best_epoch=39)
         self.assertEqual(test_obj.earlyStopping.mode, recording)
 
         # Check if the best epochs are recorded with mode RECORDING using the decorator
@@ -886,19 +886,19 @@ class TestEarlyStopping(ModelDataSetsMixIn, TestCase):
 
         # Check if the best epochs are not recorded with other modes using the decorator
         test_obj.earlyStopping.mode = EarlyStoppingMode.FIXED
-        _ = test_obj.test_func(None, None, None, None, best_epoch=49)
+        _ = test_obj.test_func(None, None, None, best_epoch=49)
         self.assertEqual(test_obj.earlyStopping.optimalEpochs, 35)
         test_obj.earlyStopping.mode = EarlyStoppingMode.OPTIMAL
-        _ = test_obj.test_func(None, None, None, None, best_epoch=59)
+        _ = test_obj.test_func(None, None, None, best_epoch=59)
         self.assertEqual(test_obj.earlyStopping.optimalEpochs, 35)
         test_obj.earlyStopping.mode = EarlyStoppingMode.NOT_RECORDING
-        _ = test_obj.test_func(None, None, None, None, best_epoch=69)
+        _ = test_obj.test_func(None, None, None, best_epoch=69)
         self.assertEqual(test_obj.earlyStopping.optimalEpochs, 35)
 
         # check decorator raises error when early stopping is not supported
         test_obj = test_class(support=False)
         with self.assertRaises(AssertionError):
-            _ = test_obj.test_func(None, None, None, None, best_epoch=40)
+            _ = test_obj.test_func(None, None, None, best_epoch=40)
 
 
 class TestMonitors(ModelDataSetsMixIn, ModelTestMixIn, TestCase):
