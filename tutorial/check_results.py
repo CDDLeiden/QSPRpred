7210c41126a009adb52a618a0b773955afe49c09
martin-sicho <sicho.martin@gmail.com>
2023-11-02 10:19:01 +0100

    show differences found in values if present

--- tutorial/check_results.py
+++ tutorial/check_results.py
@@ -4,10 +4,12 @@ expected results. It is not part of the tutorial itself, but is used by the
 CI/CD pipeline to check that the tutorial is up-to-date and that the models
 are consistent with previous ones.
 """
+import json
 
 import pandas as pd
 import os
 import sys
+import traceback
 
 
 success = True
@@ -37,11 +39,29 @@ for f in os.listdir("expected"):
             assert expected_values.index.equals(
                 actual_values.index
             ), f"Index values do not match for file {file_name}."
-            assert expected_values.equals(
-                actual_values
-            ), f"Values do not match for file {file_name}."
+            try:
+                assert expected_values.equals(
+                    actual_values
+                ), f"Values do not match for file {file_name}."
+            except AssertionError as e:
+                sys.stderr.write(f"Comparison error in values of: {file_name}\n")
+                # check and print which values are different
+                diff = expected_values.compare(actual_values)
+                for idx, row in diff.iterrows():
+                    sys.stderr.write(f"Found differences for item: {idx}\n")
+                    overview = dict()
+                    for col in diff.columns:
+                        name = col[0]
+                        overview[name] = dict()
+                        if col[1] == "self":
+                            overview[name]["self"] = row[col]
+                        elif col[1] == "other":
+                            overview[name]["other"] = row[col]
+                    sys.stderr.write(json.dumps(overview, indent=4))
+                raise e
         except AssertionError as e:
-            sys.stderr.write(f"Comparison error in: {f}\n")
+            # print  stack trace
+            traceback.print_exc()
             success = False
             continue
         print("Comparison successful!")
