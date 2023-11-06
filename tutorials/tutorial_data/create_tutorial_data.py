b4beb6ac6c9acf041983c9d01f95c9ace68a3995
maagdenbehwvanden1 <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2023-11-06 14:45:19 +0100

    add model assessment and data splitting tutorial

--- tutorials/tutorial_data/create_tutorial_data.py
+++ tutorials/tutorial_data/create_tutorial_data.py
@@ -1,17 +1,20 @@
 import os
 
-from qsprpred.data.data import QSPRDataset
 from qsprpred.data.sources.papyrus import Papyrus
-from qsprpred.models.tasks import TargetTasks
 
-
-def A2AR(data_dir="tutorial_data", random_state=None):
-    """A classification dataset that contains activity data on the adenosine A2A
+def A2AR(data_dir: str | None = None, random_state: int = 42):
+    """Creates a dataset that contains activity data on the adenosine A2A
     receptor loaded from the Papyrus database using the built-in Papyrus wrapper.
 
+    Args:
+        data_dir: directory where the dataset will be saved, if None, the
+            tutorial_data directory will be used
+        random_state: random state for reproducibility
+
     Returns:
-        a `QSPRDataset` instance with the loaded data
+        a `MolTable` instance with the loaded data
     """
+    data_dir = os.path.dirname(__file__) if data_dir is None else data_dir
     acc_keys = [
         "P29274"
     ]  # Adenosine receptor A2A (https://www.uniprot.org/uniprotkb/P29274/entry)
@@ -33,23 +36,12 @@ def A2AR(data_dir="tutorial_data", random_state=None):
     print(f"Number of samples loaded: {len(dataset.getDF())}")
 
     df = dataset.getDF()
-    df = df[["SMILES", "pchembl_value_Mean"]]
+    df = df[["SMILES", "pchembl_value_Mean", "Year"]]
     df.to_csv(
         os.path.join(data_dir, "A2A_LIGANDS.tsv"), index=False, sep="\t"
     )
 
-    return QSPRDataset.fromMolTable(
-        dataset,
-        [
-            {
-                "name": "pchembl_value_Median",
-                "task": TargetTasks.SINGLECLASS,
-                "th": [6.5],
-            }
-        ],
-        overwrite=True,
-        random_state=random_state,
-    )
+    return dataset
 
 
 if __name__ == "__main__":
