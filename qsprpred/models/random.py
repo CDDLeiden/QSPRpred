826e78d91a4ae87e3ad51a645ff5382735cc3bfe
chichi148 <jespersc@xs4all.nl>
2023-12-22 14:00:23 +0100

    Singletask regression working for RandomModel! Needs commenting out of alg lines though...

--- qsprpred/models/random.py
+++ qsprpred/models/random.py
@@ -7,6 +7,10 @@ from .models import QSPRModel
 from ..data.tables.qspr import QSPRDataset
 from typing import Any
 
+import jsonpickle
+
+jsonpickle.set_encoder_options('json', indent=4)
+
 class RandomModel(QSPRModel):
     def __init__(self,
         base_dir: str,
@@ -21,6 +25,8 @@ class RandomModel(QSPRModel):
         self.name = name
         self.parameters = parameters
         self.estimator = self.loadEstimator(self.parameters)
+        self.alg = None
+        super().initFromData(data)
 
     def fit(
         self,
@@ -39,15 +45,14 @@ class RandomModel(QSPRModel):
 
             # TODO: this is probably clumsy, I shouldn't have to go from dataframe
             # to dict to dataframe
-            estimator.ratios = pd.DataFrame.from_dict({col: y[col].value_counts() / y.shape[0] for col in list(y)})
+            estimator["ratios"] = pd.DataFrame.from_dict({col: y[col].value_counts() / y.shape[0] for col in list(y)})
 
         if (self.task.isRegression()):
             # Calculate the mean and standard deviation of each column
-            estimator.mean = y.mean()
-            estimator.std = y.std()
+            estimator["mean"] = y.mean()
+            estimator["std"] = y.std()
         
         self.estimator = estimator
-        print(self.estimator)
 
     def predict(
         self, X: pd.DataFrame | np.ndarray | QSPRDataset, estimator: Any = None
@@ -55,14 +60,16 @@ class RandomModel(QSPRModel):
         # Values of X are irrelevant
         # TODO: might need to be len(X.T)
         # TODO: clumsy
-        print(self.estimator)
 
         if (self.task.isClassification()):
-            y_list = [pd.DataFrame(np.random.choice(estimator.ratios.shape[0], len(X), p=estimator.ratios[col]), columns=[col]) for col in list(estimator.ratios)]
+            y_list = [pd.DataFrame(np.random.choice(self.estimator["ratios"].shape[0], len(X), p=self.estimator["ratios"][col]), columns=[col]) for col in list(self.estimator["ratios"])]
             return pd.concat(y_list, axis=1)
         if (self.task.isRegression()):
-            y_list = [pd.DataFrame(np.random.normal(loc=estimator.mean[col], scale=estimator.std[col], size=len(X)), columns=[col]) for col in len(estimator.mean)]
-            return pd.concat(y_list, axis=1)
+            y_list = [pd.DataFrame(np.random.normal(loc=self.estimator["mean"][col], scale=self.estimator["std"][col], size=len(X)), columns=[col]) for col in range(len(self.estimator["mean"]))]
+            y = pd.concat(y_list, axis=1).to_numpy()
+            if y.ndim == 1:
+                y = y.reshape(-1, 1)
+            return y
 
     def predictProba(
         self, X: pd.DataFrame | np.ndarray | QSPRDataset, estimator: Any = None
@@ -72,12 +79,11 @@ class RandomModel(QSPRModel):
         # TODO: clumsy
 
         if (self.task.isClassification()):
-            y_list = [pd.DataFrame(np.random.choice(estimator.ratios.shape[0], len(X), p=estimator.ratios[col]), columns=[col]) for col in list(estimator.ratios)]
+            y_list = [pd.DataFrame(np.random.choice(estimator["ratios"].shape[0], len(X), p=estimator["ratios"][col]), columns=[col]) for col in list(estimator["ratios"])]
             return pd.concat(y_list, axis=1)
         if (self.task.isRegression()):
-            y_list = [pd.DataFrame(np.random.normal(loc=estimator.mean[col], scale=estimator.std[col], size=len(X)), columns=[col]) for col in len(estimator.mean)]
+            y_list = [pd.DataFrame(np.random.normal(loc=estimator["mean"][col], scale=estimator["std"][col], size=len(X)), columns=[col]) for col in list(estimator["mean"])]
             return pd.concat(y_list, axis=1)
-
 
     @property
     def supportsEarlyStopping(self) -> bool:
@@ -93,11 +99,31 @@ class RandomModel(QSPRModel):
 
 
     def loadEstimatorFromFile(self, params: dict | None = None) -> object:
-        pass
+        path = f"{self.outPrefix}.json"
+        if os.path.isfile(path):
+            with open(path, 'r') as f:
+                estimator = self.estimatorFromJson(f.read())
+            new_parameters = self.getParameters(params)
+            if new_parameters is not None:
+                return estimator.set_params(**new_parameters)
+            else:
+                return estimator
+        else:
+            raise FileNotFoundError(
+                f"No estimator found at {path}, loading estimator from file failed."
+            )
+
     
     def saveEstimator(self) -> str:
         """See `QSPRModel.saveEstimator`."""
         estimator_path = f"{self.outPrefix}.json"
         with open(estimator_path, 'w') as f:
-            f.write(json.dumps(self.estimator, indent=4))
+            f.write(self.estimatorToJson())
         return estimator_path
+
+    def estimatorToJson(self) -> str:
+        return jsonpickle.encode(self.estimator, unpicklable=True)
+    
+    def estimatorFromJson(self, json):
+        return jsonpickle.decode(json)
+    
