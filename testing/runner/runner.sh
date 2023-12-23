91f12c87daea0e1b306be15cddb492cb64884216
Martin Sicho <sicho.martin@gmail.com>
2023-12-23 17:04:02 +0100

    add custom project name

--- testing/runner/runner.sh
+++ testing/runner/runner.sh
@@ -2,13 +2,13 @@
 
 export TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
-docker-compose rm -f
+docker-compose -p "test-runner-${TIMESTAMP}" rm -f
 echo "Spawning Runner... TIMESTAMP=${TIMESTAMP}"
 mkdir -p logs
-docker-compose config >> "logs/${TIMESTAMP}.log"
-docker-compose up --force-recreate
-docker-compose logs >> "logs/${TIMESTAMP}.log"
+docker-compose -p "test-runner-${TIMESTAMP}" config >> "logs/${TIMESTAMP}.log"
+docker-compose -p "test-runner-${TIMESTAMP}" up --force-recreate
+docker-compose -p "test-runner-${TIMESTAMP}" logs >> "logs/${TIMESTAMP}.log"
 echo "Logs saved in logs/${TIMESTAMP}.log"
 echo "Cleaning up..."
-docker-compose rm -f
+docker-compose -p "test-runner-${TIMESTAMP}" rm -f
 docker image rm "test-runner-${TIMESTAMP}"
