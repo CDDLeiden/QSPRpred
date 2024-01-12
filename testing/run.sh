c96c4814bcbc9a60bf4ace22b6651244b8858742
martin-sicho <sicho.martin@gmail.com>
2024-01-12 14:38:01 +0100

    run consistency before tutorials

--- testing/run.sh
+++ testing/run.sh
@@ -12,5 +12,5 @@ pytest -xv ../qsprpred/extra/gpu
 pytest -xv ../qsprpred/extra/models
 pytest -xv ../qsprpred/plotting
 cd test_cli && ./run.sh && cd "$WD"
-cd test_tutorial && ./run.sh && cd "$WD"
 cd test_consistency && ./run.sh && cd "$WD"
+cd test_tutorial && ./run.sh && cd "$WD"
