6cd434ebd705d340079f3c734b573f3bec994565
martin-sicho <sicho.martin@gmail.com>
2024-01-16 16:54:42 +0100

    add environment variable for optional tutorial tests

--- testing/run.sh
+++ testing/run.sh
@@ -4,6 +4,7 @@ set -e
 
 WD=$(pwd)
 RUN_EXTRAS=${QSPPRED_TEST_EXTRAS:-true}
+RUN_TUTORIAL=${QSPPRED_TEST_TUTORIAL:-true}
 echo "Running tests with RUN_EXTRAS=$RUN_EXTRAS"
 
 # -x: stop on first failure, -v: verbose
@@ -12,12 +13,14 @@ pytest -xv ../qsprpred/data
 pytest -xv ../qsprpred/plotting
 # only run extras if explicitly requested
 if [ "$RUN_EXTRAS" == "true" ]; then
-    pytest -xv ../qsprpred/extra/data/descriptors
-    pytest -xv ../qsprpred/extra/data/sampling
-    pytest -xv ../qsprpred/extra/data/tables
-    pytest -xv ../qsprpred/extra/gpu
-    pytest -xv ../qsprpred/extra/models
+  pytest -xv ../qsprpred/extra/data/descriptors
+  pytest -xv ../qsprpred/extra/data/sampling
+  pytest -xv ../qsprpred/extra/data/tables
+  pytest -xv ../qsprpred/extra/gpu
+  pytest -xv ../qsprpred/extra/models
 fi
 cd test_cli && ./run.sh && cd "$WD"
 cd test_consistency && ./run.sh && cd "$WD"
-cd test_tutorial && ./run.sh && cd "$WD"
+if [ "$RUN_TUTORIAL" == "true" ]; then
+  cd test_tutorial && ./run.sh && cd "$WD"
+fi
