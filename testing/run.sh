1f5cb0bc2bc3779475889434694fd8c632ffbdc3
martin-sicho <sicho.martin@gmail.com>
2024-01-20 21:04:41 +0100

    move pytest commands to separate script

--- testing/run.sh
+++ testing/run.sh
@@ -3,27 +3,18 @@
 set -e
 
 WD=$(pwd)
-RUN_EXTRAS=${QSPPRED_TEST_EXTRAS:-true}
-RUN_TUTORIAL=${QSPPRED_TEST_TUTORIAL:-true}
+export QSPPRED_TEST_TUTORIAL=${QSPPRED_TEST_TUTORIAL:-true}
+export QSPPRED_TEST_EXTRAS=${QSPPRED_TEST_EXTRAS:-true}
+export QSPPRED_PYTEST_ARGS=${QSPPRED_PYTEST_ARGS:-""}
 echo "Running tests with:"
-echo "  RUN_EXTRAS=$RUN_EXTRAS"
-echo "  RUN_TUTORIAL=$RUN_TUTORIAL"
+echo "QSPPRED_TEST_TUTORIAL=$QSPPRED_TEST_TUTORIAL"
+echo "QSPPRED_TEST_EXTRAS=$QSPPRED_TEST_EXTRAS"
+echo "QSPPRED_PYTEST_ARGS=$QSPPRED_PYTEST_ARGS"
 
-# -x: stop on first failure, -v: verbose
-pytest -xv ../qsprpred/benchmarks
-pytest -xv ../qsprpred/data
-pytest -xv ../qsprpred/plotting
-# only run extras if explicitly requested
-if [ "$RUN_EXTRAS" == "true" ]; then
-  pytest -xv ../qsprpred/extra/data/descriptors
-  pytest -xv ../qsprpred/extra/data/sampling
-  pytest -xv ../qsprpred/extra/data/tables
-  pytest -xv ../qsprpred/extra/gpu
-  pytest -xv ../qsprpred/extra/models
-fi
+cd test_pytest && ./run.sh && cd "$WD"
 cd test_cli && ./run.sh && cd "$WD"
 cd test_tutorial && ./prepare.sh && cd "$WD"
 cd test_consistency && ./run.sh && cd "$WD"
-if [ "$RUN_TUTORIAL" == "true" ]; then
+if [ "$QSPPRED_TEST_TUTORIAL" == "true" ]; then
   cd test_tutorial && ./run.sh && cd "$WD"
 fi
