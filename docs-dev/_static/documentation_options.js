3ba20f34aff6798b7c39fb3c53f2c8681e859ab7
Mw. H.W. van den Maagdenberg MSc <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2024-02-26 17:00:06 +0000

    Adding docs to gh_pages for 2e4cf644

--- docs-dev/_static/documentation_options.js
+++ docs-dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0.0.dev502+gb28f8439',
+    VERSION: '3.0.0.dev506+g2e4cf644',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
