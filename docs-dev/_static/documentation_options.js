52392d44f08d504b3ec9fc6c01105d3a5390edc3
Dhr. Dr.ing. M. Sicho <sicho.martin@gmail.com>
2024-06-18 12:50:16 +0000

    Adding docs to gh_pages for 047ce537

--- docs-dev/_static/documentation_options.js
+++ docs-dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.1a2.dev59+ge430e991',
+    VERSION: '3.1.1a2.dev65+g047ce537',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
