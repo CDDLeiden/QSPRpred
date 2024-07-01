48508e5312ba4f56c8d9eefb24110422629fd6ea
martin-sicho <sicho.martin@gmail.com>
2024-07-01 13:18:44 +0000

    Adding docs to gh_pages for 74780862

--- docs-dev/_static/documentation_options.js
+++ docs-dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.1a2.dev69+g897e33ce',
+    VERSION: '3.1.1a2.dev70+g74780862',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
