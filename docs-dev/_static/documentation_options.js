71104bba15422a12ad07159efd7d2827ec7fdf13
martin-sicho <sicho.martin@gmail.com>
2024-07-01 14:27:04 +0000

    Adding docs to gh_pages for 2e1f75f1

--- docs-dev/_static/documentation_options.js
+++ docs-dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.1a2.dev71+g35ad93b0',
+    VERSION: '3.1.1a2.dev72+g2e1f75f1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
