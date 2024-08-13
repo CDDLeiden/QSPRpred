4360abb8c12e85de0886140932a17a7e5f742757
Helle van den Maagdenberg <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2024-08-13 15:19:07 +0200

    move calibration error to Metrics interface

--- qsprpred/models/metrics.py
+++ qsprpred/models/metrics.py
@@ -7,7 +7,6 @@ import sklearn
 from sklearn.metrics import get_scorer
 from sklearn.metrics._scorer import _BaseScorer
 
-
 class Metric(ABC):
     """Abstract class for scoring functions.
 
@@ -106,78 +105,97 @@ class SklearnMetrics(Metric):
         )
 
 
-def calibration_error(
-    y_true: np.array, y_prob: np.array, n_bins: int = 10, norm: str = "L1"
-) -> float:
+class CalibrationError(Metric):
     """Compute the calibration error of a classifier.
-
+    
     ECE is defined as the expected difference between the predicted probability
     and the observed frequency in each bin. The lower the ECE, the more
     calibrated the classifier is.
-
-    If `y_prob` is 1d, it assumes that probablities correspond to the positive class of
-    a binary classification. If `y_prob` is 2d, it assumes each column corresponds to a
-    class and that the classes are in the columns in ascending order.
-
-    If `norm` is 'L1', the expected calibration error is returned (ECE).
-    If `norm` is 'L2', the root-mean-square calibration error is returned (RMSCE).
-    If `norm` is 'infinity', the maximum calibration error is returned (MCE).
-
-    Referece: Guo et al. (2017) On Calibration of Modern Neural Networks. https://arxiv.org/abs/1706.04599
-
-    Args:
-        y_true (np.array): True class labels. 1d array.
-        y_prob (np.array): Raw probability/score of the positive class
-            or multiclass probability. 1d or 2d array.
-        n_bins (int): Number of bins to use for calibration.
-            A bigger bin number requires more data. Defaults to 10.
-        norm (str): The norm to use for the calibration error.
-            Can be 'L1' or 'L2' or 'infinity'. Defaults to 'L1'.
-
-    Returns:
-        float: The calibration error.
+    
+    Referece: Guo et al. (2017) On Calibration of Modern Neural Networks.
+    https://arxiv.org/abs/1706.04599
+    
+    Attributes:
+        name (str): Name of the scoring function (calibration_error).
     """
-    y_true = np.array(y_true)
-    y_prob = np.array(y_prob)
-    # If `y_prob` is 1d, convert it to 2d by adding
-    # a new axis corresponding to the negative class
-    if y_prob.ndim == 1:
-        y_prob_0 = 1 - y_prob
-        y_prob = np.stack([y_prob_0, y_prob], axis=1)
-    # Get the highest probability and the predicted class
-    y_prob_max = np.max(y_prob, axis=1)
-    y_pred_class = np.argmax(y_prob, axis=1)
-    # Sort data based on the highest probability
-    sorted_indices = np.argsort(y_prob_max)
-    sorted_y_true = y_true[sorted_indices]
-    sorted_y_prob_max = y_prob_max[sorted_indices]
-    sorted_y_pred_class = y_pred_class[sorted_indices]
-    # Bin sorted data
-    binned_y_true = np.array_split(sorted_y_true, n_bins)
-    binned_y_prob_max = np.array_split(sorted_y_prob_max, n_bins)
-    binned_y_pred_class = np.array_split(sorted_y_pred_class, n_bins)
-    # Compute the calibration error by iterating over the bins
-    calibration_error = 0.0
-    for bin_y_true, bin_y_prob_max, bin_y_pred_class in zip(
-        binned_y_true, binned_y_prob_max, binned_y_pred_class
-    ):
-        # Compute the accuracy and the mean probability for the bin
-        mean_prob = np.mean(bin_y_prob_max)
-        accuracy = np.mean(bin_y_true == bin_y_pred_class)
-        # Compute the calibration error for the bin based on the norm
-        if norm == "L1":
-            calibration_error += (
-                np.abs(mean_prob - accuracy) * len(bin_y_true) / len(y_true)
-            )
-        elif norm == "L2":
-            calibration_error += (
-                np.square(mean_prob - accuracy)**2 * len(bin_y_true) / len(y_true)
-            )
-        elif norm == "infinity":
-            calibration_error = max(calibration_error, np.abs(mean_prob - accuracy))
-        else:
-            raise ValueError(f"Unknown norm {norm}")
-    if norm == "L2":
-        calibration_error = np.sqrt(calibration_error)
 
-    return calibration_error
+    def __call__(
+        self,
+        y_true: np.array,
+        y_pred: list[np.ndarray],
+        n_bins: int = 10,
+        norm: str = "L1"
+    ) -> float:
+        """Compute the calibration error of a classifier.
+
+        If `norm` is 'L1', the expected calibration error is returned (ECE).
+        If `norm` is 'L2', the root-mean-square calibration error is returned (RMSCE).
+        If `norm` is 'infinity', the maximum calibration error is returned (MCE).
+
+        Referece: Guo et al. (2017) On Calibration of Modern Neural Networks.
+        https://arxiv.org/abs/1706.04599
+
+        Args:
+            y_true (np.array): True class labels. 1d array.
+            y_pred (list[np.array]): Predicted class probabilities.
+                List of arrays of shape (n_samples, n_classes) of length n_tasks.
+                Note. Multi-task predictions are not supported.
+            n_bins (int): Number of bins to use for calibration.
+                A bigger bin number requires more data. Defaults to 10.
+            norm (str): The norm to use for the calibration error.
+                Can be 'L1' or 'L2' or 'infinity'. Defaults to 'L1'.
+
+        Returns:
+            float: The calibration error.
+        """
+        # Check if y_pred is a list of arrays of length 1
+        if not isinstance(y_pred, list):
+            raise ValueError("y_pred must be a list of 2D arrays.")
+        if len(y_pred) > 1:
+            raise ValueError("Multi-task predictions are not supported.")
+        
+        # TODO: support multi-task predictions
+        # Convert y_pred to a 2D array 
+        y_pred = y_pred[0]
+
+        # Get the highest probability and the predicted class
+        y_pred_max = np.max(y_pred, axis=1)
+        y_pred_class = np.argmax(y_pred, axis=1)
+        # Sort data based on the highest probability
+        sorted_indices = np.argsort(y_pred_max)
+        sorted_y_true = y_true[sorted_indices]
+        sorted_y_pred_max = y_pred_max[sorted_indices]
+        sorted_y_pred_class = y_pred_class[sorted_indices]
+        # Bin sorted data
+        binned_y_true = np.array_split(sorted_y_true, n_bins)
+        binned_y_pred_max = np.array_split(sorted_y_pred_max, n_bins)
+        binned_y_pred_class = np.array_split(sorted_y_pred_class, n_bins)
+        # Compute the calibration error by iterating over the bins
+        calibration_error = 0.0
+        for bin_y_true, bin_y_pred_max, bin_y_pred_class in zip(
+            binned_y_true, binned_y_pred_max, binned_y_pred_class
+        ):
+            # Compute the accuracy and the mean probability for the bin
+            mean_prob = np.mean(bin_y_pred_max)
+            accuracy = np.mean(bin_y_true == bin_y_pred_class)
+            # Compute the calibration error for the bin based on the norm
+            if norm == "L1":
+                calibration_error += (
+                    np.abs(mean_prob - accuracy) * len(bin_y_true) / len(y_true)
+                )
+            elif norm == "L2":
+                calibration_error += (
+                    np.square(mean_prob - accuracy)**2 * len(bin_y_true) / len(y_true)
+                )
+            elif norm == "infinity":
+                calibration_error = max(calibration_error, np.abs(mean_prob - accuracy))
+            else:
+                raise ValueError(f"Unknown norm {norm}")
+        if norm == "L2":
+            calibration_error = np.sqrt(calibration_error)
+
+        return calibration_error
+    
+    def __str__(self) -> str:
+        """Return the name of the scorer."""
+        return "calibration_error"
