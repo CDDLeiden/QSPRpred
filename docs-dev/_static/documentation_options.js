225bd21b9d4d56b1c1d48b8fd29cbc2804d92c89
Helle van den Maagdenberg <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2024-08-26 14:40:08 +0000

    Adding docs to gh_pages for be4ae447

--- docs-dev/_static/documentation_options.js
+++ docs-dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.2.dev6+g4022298a',
+    VERSION: '3.1.2.dev37+gbe4ae447',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
