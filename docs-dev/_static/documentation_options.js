4830b0c40aa52df7b18f43b87c314b72d9b7cfbf
Dhr. Dr.ing. M. Sicho <sicho.martin@gmail.com>
2024-09-05 14:55:27 +0000

    Adding docs to gh_pages for 5c50f5f6

--- docs-dev/_static/documentation_options.js
+++ docs-dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.2.dev40+g2513259a',
+    VERSION: '3.2.1.dev95+g5c50f5f6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
