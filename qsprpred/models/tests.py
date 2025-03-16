0962f1a05f0c17993d283eb7cf55be3f661747c4
Helle van den Maagdenberg <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2025-03-16 11:12:46 +0100

    fix test multitask

--- qsprpred/models/tests.py
+++ qsprpred/models/tests.py
@@ -443,13 +443,13 @@ class TestSklearnClassificationMultiTask(SklearnBaseModelTestCase):
                     "name": "fu",
                     "task": TargetTasks.SINGLECLASS,
                     "th": [0.3],
-                    "imputer": SimpleImputer(strategy="mean"),
+                    "imputer": SimpleImputer(strategy="most_frequent"),
                 },
                 {
                     "name": "CL",
                     "task": TargetTasks.SINGLECLASS,
                     "th": [6.5],
-                    "imputer": SimpleImputer(strategy="mean"),
+                    "imputer": SimpleImputer(strategy="most_frequent"),
                 },
             ],
         )
