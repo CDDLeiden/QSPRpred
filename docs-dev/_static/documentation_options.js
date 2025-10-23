b1b2de01b2849e8f9022736bc2699f48dc5c9d02
H.W. van den Maagdenberg MSc <h.w.van.den.maagdenberg@lacdr.leidenuniv.nl>
2025-10-23 07:31:02 +0000

    Adding docs to gh_pages for aa123e9e

--- docs-dev/_static/documentation_options.js
+++ docs-dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.0.0a2.dev3+gdd1d34be',
+    VERSION: '4.0.0a2.dev175+gaa123e9e2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
