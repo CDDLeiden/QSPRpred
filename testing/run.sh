b9a6909296a1b6cd6bba93516470156f41e570ad
Martin Sicho <sichom@vscht.cz>
2026-06-18 14:11:32 +0200

    add switches for tests subsets

--- testing/run.sh
+++ testing/run.sh
@@ -5,9 +5,17 @@ set -e
 WD=$(pwd)
 export QSPPRED_TEST_TUTORIAL=${QSPPRED_TEST_TUTORIAL:-true}
 echo "Setting QSPPRED_TEST_TUTORIAL=$QSPPRED_TEST_TUTORIAL"
+export QSPPRED_TEST_CLI=${QSPPRED_TEST_CLI:-true}
+echo "Setting QSPPRED_TEST_CLI=$QSPPRED_TEST_CLI"
+export QSPPRED_TEST_PYTEST=${QSPPRED_TEST_PYTEST:-true}
+echo "Setting QSPPRED_TEST_PYTEST=$QSPPRED_TEST_PYTEST"
 
-cd test_pytest && ./run.sh && cd "$WD"
-cd test_cli && ./run.sh && cd "$WD"
+if [ "$QSPPRED_TEST_PYTEST" == "true" ]; then
+  cd test_pytest && ./run.sh && cd "$WD"
+fi
+if [ "$QSPPRED_TEST_CLI" == "true" ]; then
+  cd test_cli && ./run.sh && cd "$WD"
+fi
 cd test_tutorial && ./prepare.sh && cd "$WD"
 cd test_consistency && ./run.sh && cd "$WD"
 if [ "$QSPPRED_TEST_TUTORIAL" == "true" ]; then
