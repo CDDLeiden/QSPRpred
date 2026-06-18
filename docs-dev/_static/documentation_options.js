84a5ee39b713e818b92459d363d224c03f27cda9
Martin Šícho <m.sicho@lacdr.leidenuniv.nl>
2026-06-18 19:09:57 +0000

    Adding docs to gh_pages for d638acbe

--- docs-dev/_static/documentation_options.js
+++ docs-dev/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.0.0a2.dev175+gaa123e9e2',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '4.0.0a3.dev42+gd638acbe9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
